534a1682b1ade10c6843eb44e6bb55b5b443bf7a
xielbers <88589081+xielbers@users.noreply.github.com>
2024-06-18 17:42:30 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function CurrentDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = ('0' + (today.getMonth() + 1)).slice(-2);
+    const day = ('0' + today.getDate()).slice(-2);
+    document.getElementById('date').value = `${year}-${month}-${day}`;
+}
+
+function CurrentTime() {
+    const now = new Date();
+    const hours = ('0' + now.getHours()).slice(-2);
+    const minutes = ('0' + now.getMinutes()).slice(-2);
+    document.getElementById('time').value = `${hours}:${minutes}`;
+}
+
 function Timestamp() {
     const dateInput = document.getElementById('date').value;
     const timeInput = document.getElementById('time').value;
@@ -7,7 +22,9 @@ function Timestamp() {
         const timestamp = new Date(dateTimeString).getTime() / 1000;
         
         document.getElementById('result').innerText = 'Timestamp: ' + timestamp;
+        document.getElementById('time').innerText = convertTimestamp(timestamp);
     } else {
-        document.getElementById('result').innerText = 'Por favor, selecciona una fecha y hora válidas.';
+        document.getElementById('timestampResult').innerText = 'Por favor, selecciona una fecha y hora válidas.';
+        document.getElementById('time').innerText = '';
     }
 }
